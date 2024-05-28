b9a3444dbf15f4ad3049a536a5ec2c0cc777f58c
rushilpatel21 <rushilpatel210@gmail.com>
2024-05-28 21:33:01 +0530

    Certificate Page

--- portfolio/src/pages/certificate.jsx
+++ portfolio/src/pages/certificate.jsx
@@ -1,14 +1,30 @@
 import '../styles/certificate.css';
 
-const Certificate = () => {
+const certificatesData = [
+  {
+    name: '1x Academic Excellence Certificate (Nirma University)',
+    description: 'Awarded for scoring 9.43 SGPA in SEM - III',
+  },
+  {
+    name: '2x Certificate of Scholar (Nirma University)',
+    description: 'Awarded for scoring 8.57 and 8.83 SGPA in SEM - I and II respectively',
+  }
+];
+
+const Certificates = () => {
   return (
-    <>
-        <div className='certificate-page' id='certificates'>
-             
-        </div>
-        
-    </>
+    <section className="certificates-section certificates-page" id="certificates">
+      <h1>My Certificates</h1>
+      <div className="certificates-container">
+        {certificatesData.map((certificate, index) => (
+          <div key={index} className="certificate-card">
+            <h3>{certificate.name}</h3>
+            <p><em>{certificate.description}</em></p>
+          </div>
+        ))}
+      </div>
+    </section>
   );
 };
 
-export default Certificate;
+export default Certificates;
