5ed29947f272f91d8d26e81f28fe662910e3268c
rushilpatel21 <rushilpatel210@gmail.com>
2024-05-30 00:22:39 +0530

    added tools in skills.jsx

--- portfolio/src/pages/skills.jsx
+++ portfolio/src/pages/skills.jsx
@@ -4,7 +4,7 @@ const skillsData = {
   Languages: ['C++', 'Java', 'JavaScript', 'Python'],
   Frameworks: ['React', 'Node.js', 'Express'],
   Databases: ['MongoDB', 'SQL*Plus'],
-  Tools: ['VS Code', 'Postman','Git']
+  Tools: ['VS Code', 'Postman','Git','MongoDBCompass','Jupyter Notebook','IntelliJ Idea','Matlab','Google Colab']
 };
 
 const Skills = () => {
