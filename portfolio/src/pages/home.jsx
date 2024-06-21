28aae7d0645903d9c64f6040cc5f4f75b86c718a
rushilpatel21 <rushilpatel210@gmail.com>
2024-06-21 23:00:21 +0530

    Changed the icons

--- portfolio/src/pages/home.jsx
+++ portfolio/src/pages/home.jsx
@@ -1,17 +1,15 @@
 import '../styles/home.css';
-import githubIcon from '../assets/github.png'; 
-import linkedInIcon from '../assets/linkedin.png';
-import discordIcon from '../assets/discord.png';
-import downloadIcon from '../assets/resume.png';
 import rushilResume from '../assets/Rushil\'s Resume.pdf';
-
+import Download from '../assets/download.svg';
+import github from '../assets/github.svg';
+import linkedin from '../assets/linkedin.svg';
+import discord from '../assets/discord.svg';
 
 const Home = () => {
 
     const downloadResume = () => {
         fetch(rushilResume).then((response) => {
             response.blob().then((blob) => {
-             
                 const fileURL = window.URL.createObjectURL(blob);
                 let alink = document.createElement("a");
                 alink.href = fileURL;
@@ -21,31 +19,29 @@ const Home = () => {
         });
     }
 
-  return (
-    <>
-        <div className='home-page' id='home'>
-            <div className='Home-Name'>Rushil Patel</div>
-            <div className='home-social-media'>
-                <a href="https://github.com/rushilpatel21" target="_blank" rel="noopener noreferrer" className='home-social-icon'>
-                    <img src={githubIcon} alt="Icon" style={{ width: 52, height: "auto" }}/>
-                </a>
-                <a href="https://www.linkedin.com/in/rushilpatel21/" target="_blank" rel="noopener noreferrer" className='home-social-icon'>
-                    <img src={linkedInIcon} alt="Icon" style={{ width: 52, height: "auto" }}/>
-                </a>
-                <a href="https://discordapp.com/users/617247545923469313" target="_blank" rel="noopener noreferrer" className='home-social-icon'>
-                    <img src={discordIcon} alt="Icon" style={{ width: 52, height: "auto" }}/>
-                </a>
-                <a className="home-social-icon">
-                    <button onClick={downloadResume}>
-                        <img src={downloadIcon} alt="Icon" />
-                    </button>
-                </a>
-                
-            </div> 
-        </div>
-        
-    </>
-  );
+    return (
+        <>
+            <div className='home-page' id='home'>
+                <div className='Home-Name'>Rushil Patel</div>
+                <div className='home-social-media'>
+                    <a href="https://github.com/rushilpatel21" target="_blank" rel="noopener noreferrer" className='home-social-icon'>
+                        <img src={github} alt="Github Icon" style={{ width: 40, height: "auto" }}/>
+                    </a>
+                    <a href="https://www.linkedin.com/in/rushilpatel21/" target="_blank" rel="noopener noreferrer" className='home-social-icon'>
+                        <img src={linkedin} alt="LinkedIn Icon" style={{ width: 40, height: "auto" }}/>
+                    </a>
+                    <a href="https://discordapp.com/users/617247545923469313" target="_blank" rel="noopener noreferrer" className='home-social-icon'>
+                        <img src={discord} alt="Discord Icon" style={{ width: 40, height: "auto" }}/>
+                    </a>
+                    <a className="home-social-icon">
+                        <button onClick={downloadResume}>
+                        <img src={Download} alt="Discord Icon" style={{ width: 40, height: "auto" }}/>
+                        </button>
+                    </a>
+                </div>
+            </div>
+        </>
+    );
 };
 
 export default Home;
